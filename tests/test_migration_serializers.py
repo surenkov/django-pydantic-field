c5ad3c8504745886bc1cceb8fea1d4976ec57f99
Savva Surenkov <savva@surenkov.space>
2023-10-19 03:27:53 +0400

    Tidy up migration serializer tests a bit

--- tests/test_migration_serializers.py
+++ tests/test_migration_serializers.py
@@ -30,14 +30,15 @@ else:
     ]
 
 
-@pytest.mark.parametrize("typ", test_types)
-def test_wrap_unwrap_idempotent(typ):
-    assert typ == GenericContainer.unwrap(GenericContainer.wrap(typ))
+@pytest.mark.parametrize("raw_type", test_types)
+def test_wrap_unwrap_idempotent(raw_type):
+    wrapped_type = GenericContainer.wrap(raw_type)
+    assert raw_type == GenericContainer.unwrap(wrapped_type)
 
 
-@pytest.mark.parametrize("typ", test_types)
-def test_serialize_eval_idempotent(typ):
-    typ = GenericContainer.wrap(typ)
-    expression, _ = MigrationWriter.serialize(GenericContainer.wrap(typ))
+@pytest.mark.parametrize("raw_type", test_types)
+def test_serialize_eval_idempotent(raw_type):
+    raw_type = GenericContainer.wrap(raw_type)
+    expression, _ = MigrationWriter.serialize(GenericContainer.wrap(raw_type))
     imports = dict(typing=t, typing_extensions=te, django_pydantic_field=django_pydantic_field)
-    assert eval(expression, imports) == typ
+    assert eval(expression, imports) == raw_type
