fda0a2beed03b0515e48fcb519efae57871556af
Savva Surenkov <savva@surenkov.space>
2025-07-06 22:27:02 +0400

    Separate v2 field tests

---
+++ tests/v2/test_fields.py
@@ -0,0 +1,22 @@
+import pytest
+
+from ..test_app.models import SampleModel
+
+_ = pytest.importorskip("django_pydantic_field.v2.fields")
+
+
+@pytest.mark.django_db
+def test_key_lookup_on_schema_field():
+    instance = SampleModel.objects.create(
+        sample_field={"stub_str": "foo", "stub_int": 1, "stub_list": ["1970-01-01"]},
+        sample_list=[],
+    )
+
+    value = SampleModel.objects.values("sample_field__stub_str").get(pk=instance.pk)
+    assert value["sample_field__stub_str"] == "foo"
+
+    value = SampleModel.objects.values("sample_field__stub_int").get(pk=instance.pk)
+    assert value["sample_field__stub_int"] == 1
+
+    value = SampleModel.objects.values("sample_field__stub_list").get(pk=instance.pk)
+    assert value["sample_field__stub_list"] == ["1970-01-01"]
